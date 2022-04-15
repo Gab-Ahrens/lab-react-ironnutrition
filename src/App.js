bec459cb04a7dc7bd5236893c299a760b6729c25
Gabriel Ahrens <gab.ahrens@gmail.com>
2022-04-15 10:38:03 +0200

    Lab incompleto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import Foods from './foods.json';
 import './App.css';
+import 'bulma/css/bulma.css';
+import FoodBox from './components/FoodBox';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <FoodBox/>
+      <FoodBox />
+      <FoodBox />
+      <FoodBox />
+      <FoodBox />
+      <FoodBox />
+      <FoodBox />
+
+
     </div>
   );
 }
